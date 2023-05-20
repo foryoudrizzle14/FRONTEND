3d8c6a499ff7c1a0ff85782d16cec37bf91b1211
Davin <davinrim10@gmail.com>
2023-05-20 13:11:39 +0900

    feat:게시판화면 수정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -4,14 +4,13 @@ import Main from '../Components/Main';
 import Test from '../Pages/DetailPage';
 import LoginForm from '../Pages/LogIn';
 import Post from '../Pages/Post';
-import Posts from '../Pages/Posts';
 import Register from '../Pages/Register';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Posts />} />
+        <Route path='/' element={<Main />} />
         <Route path='/write' element={<Post />} />
         <Route path='/LogIn' element={<LoginForm />} />
         <Route path='/register' element={<Register />} />
